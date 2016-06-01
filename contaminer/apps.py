86404566dce59ed5ba4c10f9465e643a1e750d7f
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2016-06-01 16:44:57 +0300

    Add configuration for SSH connection in apps.py

--- contaminer/apps.py
+++ contaminer/apps.py
@@ -5,3 +5,9 @@ from django.apps import AppConfig
 
 class ContaminerConfig(AppConfig):
     name = 'contaminer'
+    verbose_name = 'ContaMiner'
+    ssh_hostname = 'login.cbrc.kaust.edu.sa'
+    ssh_port = 22
+    ssh_username = 'hungleaj'
+    ssh_password = ''
+    ssh_identityfile = '/home/dunatotatos/.ssh/id_rsa3'
