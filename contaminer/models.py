6bea0163a48f71deb32b3d413b293603726e2797
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2016-06-05 10:07:39 +0300

    Add function to handle a new job and termination of a new job

--- contaminer/models.py
+++ contaminer/models.py
@@ -26,11 +26,17 @@
 
 from django.db import models
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 
 import os
 import datetime
-
+import paramiko
 import logging
+
+from .cluster import SSHChannel
 
 class Category(models.Model):
     """
@@ -71,8 +77,13 @@ class Pack(models.Model):
         of morda_prep on the contaminants list.
     """
     contaminant = models.ForeignKey(Contaminant)
+    number = models.IntegerField()  # the number assigned by morda_prep : uniq
+                                    # per contaminant
     architecture = models.CharField(max_length = 15) # dimer, domain, ...
     coverage = models.IntegerField() # in %
+
+    def __str__(self):
+        return (str(self.contaminant) + str(self.number))
 
 class Model(models.Model):
     """
@@ -85,6 +96,9 @@ class Model(models.Model):
     domain = models.IntegerField(null = True, blank = True, default = None)
     identity = models.IntegerField() # in %
     pack = models.ForeignKey(Pack)
+
+    def __str__(self):
+        return (str(self.pdb_code) + self.chain + str(self.domain))
 
 class Job(models.Model):
     """
@@ -106,7 +120,7 @@ class Job(models.Model):
                 str(self.finished) + ")"
         return res
 
-    def create(self):
+    def create(self, name, author, email):
         """Populate the fields of a job"""
         log = logging.getLogger(__name__)
         log.debug("Entering function with args :\n\
@@ -124,7 +138,95 @@ class Job(models.Model):
             self.email = email
 
         self.save()
-        log.debug("Exiting function"
+        log.debug("Exiting function")
+
+    def get_filename(self, suffix = "mtz"):
+        """Return the name of the file associated to the job"""
+        if not suffix:
+            result = "contaminer_" + str(self.id)
+        else:
+            result = "contaminer_" + str(self.id) + "." + suffix
+
+        return result
+
+    def submit(self, filepath, listpath):
+        log = logging.getLogger(__name__)
+        log.debug("Entering function with args : \n\
+                self : " + str(self) + "\n\
+                filepath : " + str(filepath) + "\n\
+                listpath : " + str(listpath))
+
+        cluster_comm = SSHChannel()
+        cluster_comm.send_file(filepath)
+        cluster_comm.send_file(listpath)
+
+        log.warning("Debug : " +
+                str(cluster_comm.launch_contaminer(os.path.basename(filepath),
+            os.path.basename(listpath))))
+
+    def terminate(self):
+        """
+            Retrieve the job from the cluster, then clean-up
+        """
+        log = logging.getLogger(__name__)
+        log.debug("Entering function with args : \n\
+                self : " + str(self))
+
+        if self.finished:
+            log.debug("Job is already finished")
+            return
+
+        cluster_comm = SSHChannel()
+        result_file = cluster_comm.get_result(self.id)
+
+        log.debug("###################")
+        with open(result_file, 'r') as f:
+            for line in f:
+                label, value, elaps_time = line[:-1].split(':')
+                uniprot_ID, ipack, space_group = label.split('_')
+                task = Task()
+                task.job = self
+                contaminant = Contaminant.objects.get(uniprot_ID = uniprot_ID)
+                pack = Pack.objects.filter(
+                        contaminant = contaminant).get(
+                                number = ipack)
+                task.pack = pack
+                task.space_group = space_group
+                task.finished = True
+
+                if value in ["error", "nosolution", "cancelled"]:
+                    task.percent = 0
+                    task.q_factor = 0
+                    task.error = (value == "error")
+                else:
+                    q_factor, percent, seq, struct, mod = value.split('-')
+                    task.q_factor = q_factor
+                    task.percent = percent
+                    task.error = False
+
+                task.save()
+
+        self.send_complete_mail()
+
+    def send_complete_mail(self):
+        current_site = Site.objects.get_current()
+        result_url = "{0}://{1}{2}".format(
+                "https",
+                current_site.domain,
+                reverse("ContaMiner:result", args=[self.id])
+                )
+
+        ctx = { 'job_name': self.name,
+                'result_link': result_url,
+                'SITE_NAME': current_site.name,
+                }
+
+        message = render_to_string("ContaMiner/email/complete_message.html",
+                ctx)
+        send_mail("Job complete", "",
+                settings.DEFAULT_MAIL, [self.email],
+                html_message = message)
+
 
 class Task(models.Model):
     """
@@ -145,3 +247,6 @@ class Task(models.Model):
     percent = models.IntegerField(default = None)
     q_factor = models.FloatField(default = None)
     exec_time = models.DurationField(default = datetime.timedelta(0))
+
+    def __str__(self):
+        return (str(self.job) + str(self.pack) + str(self.space_group))
