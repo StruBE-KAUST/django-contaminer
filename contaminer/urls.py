0bfa03e259c4f7f1868a008fa64856d3935cdfc4
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2016-06-15 14:33:56 +0300

    Add docstring

--- contaminer/urls.py
+++ contaminer/urls.py
@@ -16,6 +16,10 @@
 ##    with this program; if not, write to the Free Software Foundation, Inc.,
 ##    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+"""
+    URL configuration for contaminer
+"""
+
 from django.conf.urls import url
 from . import views
 
