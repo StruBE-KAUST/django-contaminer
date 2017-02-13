dd410e79caf83aa0715bb97f714a23b2cd85a399
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2017-02-13 16:54:00 +0300

    Update copyright in commands/terminate

--- contaminer/management/commands/terminate.py
+++ contaminer/management/commands/terminate.py
@@ -1,6 +1,6 @@
 # -*- coding : utf-8 -*-
 
-##    Copyright (C) 2015 Hungler Arnaud
+##    Copyright (C) 2017 King Abdullah University of Science and Technology
 ##
 ##    This program is free software; you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
