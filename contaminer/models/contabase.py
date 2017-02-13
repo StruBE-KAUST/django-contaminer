dcde2c3698add433732e283df0f679e27b57c7cf
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2017-02-13 12:12:54 +0300

    Add update methods to update the contabase

--- contaminer/models/contabase.py
+++ contaminer/models/contabase.py
@@ -32,6 +32,7 @@ import datetime
 import paramiko
 import logging
 import re
+import lxml.etree as ET
 
 from django.db import models
 from django.conf import settings
@@ -43,6 +44,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import ValidationError
 
 from .tools import UpperCaseCharField
+from ..ssh_tools import SSHChannel
+
 class ContaBase(models.Model):
     """
         A version of the ContaBase
@@ -73,6 +76,18 @@ class ContaBase(models.Model):
         all_objects = cls.objects.all()
         map(lambda x: x.make_obsolete(), all_objects)
 
+    @classmethod
+    def update(cls):
+        cls.make_all_obsolete()
+        new_contabase = ContaBase()
+        new_contabase.obsolete = False
+        new_contabase.save()
+        new_contabase = ContaBase.get_current()
+
+        parser = ET.XMLParser(remove_blank_text = True)
+        contabase = ET.XML(SSHChannel().get_contabase(), parser)
+        for category in contabase.iter('category'):
+            Category.update(new_contabase, category)
 
 
 class Category(models.Model):
@@ -105,6 +120,24 @@ class Category(models.Model):
         self.full_clean()
         super(Category, self).save(*args, **kwargs)
 
+    @classmethod
+    def update(cls, parent_contabase, category_dict):
+        number = int(category_dict.find('id').text)
+        new_category = Category()
+        new_category.contabase = parent_contabase
+        new_category.number = number # Unique per contabase
+        new_category.name = category_dict.find('name').text
+        default = (category_dict.find('default').text in ['true', 'True'])
+        new_category.selected_by_default = default
+        new_category.save()
+
+        new_category = Category.objects.get(
+                contabase = parent_contabase,
+                number = number,
+                )
+
+        for contaminant in category_dict.iter('contaminant'):
+            Contaminant.update(new_category, contaminant)
 
 
 class Contaminant(models.Model):
@@ -123,6 +156,32 @@ class Contaminant(models.Model):
 
     def __str__(self):
         return self.uniprot_id + ' - ' + self.short_name
+
+    @classmethod
+    def update(cls, parent_category, contaminant_dict):
+        uniprot_id = contaminant_dict.find('uniprot_id').text
+        new_contaminant = Contaminant()
+        new_contaminant.uniprot_id = uniprot_id
+        new_contaminant.category = parent_category
+        new_contaminant.short_name = contaminant_dict.find('short_name').text
+        new_contaminant.long_name = contaminant_dict.find('long_name').text
+        new_contaminant.sequence = contaminant_dict.find('sequence').text
+        new_contaminant.organism = contaminant_dict.find('organism').text
+        new_contaminant.save()
+
+        new_contaminant = Contaminant.objects.get(
+                category = parent_category,
+                uniprot_id = uniprot_id,
+                )
+
+        for pack in contaminant_dict.iter('pack'):
+            Pack.update(new_contaminant, pack)
+
+        for reference in contaminant_dict.iter('reference'):
+            Reference.update(new_contaminant, reference)
+
+        for suggestion in contaminant_dict.iter('suggestion'):
+            Suggestion.update(new_contaminant, suggestion)
 
     @staticmethod
     def get_all():
@@ -171,6 +230,23 @@ class Pack(models.Model):
         structure = str(self.structure)
         return (contaminant + ' - ' + number + ' (' + structure + ')')
 
+    @classmethod
+    def update(cls, parent_contaminant, pack_dict):
+        number = len(Pack.objects.filter(contaminant = parent_contaminant)) + 1
+        new_pack = Pack()
+        new_pack.contaminant = parent_contaminant
+        new_pack.number = number # Unique per model
+        new_pack.structure = pack_dict.find('quat_structure').text
+        new_pack.save()
+
+        new_pack = Pack.objects.get(
+                contaminant = parent_contaminant,
+                number = number,
+                )
+
+        for model in pack_dict.iter('model'):
+            Model.update(new_pack, model)
+
     def clean(self, *args, **kwargs):
         # Structure can be domain, domains, or n-mer with n integer
         if not re.match("^([1-9][0-9]*-mer|domains?)$", self.structure):
@@ -209,6 +285,17 @@ class Model(models.Model):
         pdb_code = str(self.pdb_code)
         return (pack + ' - ' + pdb_code)
 
+    @classmethod
+    def update(cls, parent_pack, model_dict):
+        new_model = Model()
+        new_model.pdb_code = model_dict.find('template').text
+        new_model.chain = model_dict.find('chain').text
+        new_model.domain = int(model_dict.find('domain').text)
+        new_model.nb_residues = int(model_dict.find('n_res').text)
+        new_model.identity = int(float(model_dict.find('identity').text)) * 100
+        new_model.pack = parent_pack
+        new_model.save()
+
     def clean(self, *args, **kwargs):
         # Number of residues should be lower than the sequence length of
         # the contaminant
@@ -240,6 +327,12 @@ class Reference(models.Model):
     def __str__(self):
         return contaminant.uniprot_id + " -> " + str(self.pubmed_id)
 
+    @classmethod
+    def update(cls, parent_contaminant, reference_dict):
+        new_reference = Reference()
+        new_reference.pubmed_id = reference_dict.find('pubmed_id').text
+        new_reference.contaminant = parent_contaminant
+        new_reference.save()
 
 
 class Suggestion(models.Model):
@@ -251,3 +344,10 @@ class Suggestion(models.Model):
 
     def __str__(self):
         return contaminant.uniprot_id + " -> " + self.name
+
+    @classmethod
+    def update(cls, parent_contaminant, suggestion_dict):
+        new_suggestion = Suggestion()
+        new_suggestion.name = suggestion_dict.find('name').text
+        new_suggestion.contaminant = parent_contaminant
+        new_suggestion.save()
