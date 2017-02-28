d3c8a1ab6b14576603387b711ef9528ebe8a3208
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2017-02-28 17:37:12 +0300

    Update job status in DB before returning to user

--- contaminer/models/contaminer.py
+++ contaminer/models/contaminer.py
@@ -77,6 +77,7 @@ class Job(models.Model):
 
     def get_status(self):
         """ Gives the status of the job as a string """
+        self.update_status()
         if self.status_error:
             return "Error"
         if self.status_complete:
