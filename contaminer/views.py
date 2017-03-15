7d6786f2d40fe93b9ea9c6c8090e1a344b9a1142
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2017-03-15 14:24:37 +0300

    Delete extra files and change Copyright

--- contaminer/views.py
+++ contaminer/views.py
@@ -1,4 +1,6 @@
-##    Copyright (C) 2016 Hungler Arnaud
+# -*- coding : utf-8 -*-
+
+##    Copyright (C) 2017 King Abdullah University of Science and Technology
 ##
 ##    This program is free software; you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
